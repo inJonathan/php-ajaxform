299f95d4e8ca59d526f8e17abf2b70c5cce459e3
Jonathan <jonrcao@gmail.com>
2016-12-24 00:00:58 +0800

    开发完成

--- data.php
+++ data.php
@@ -1,26 +1,26 @@
 <?php
 $action = $_GET['action'];
 switch($action) {
-case 'init_data_list':
-	init_data_list();
-	break;
-case 'add_row':
-	add_row();
-	break;
-case 'del_row':
-	del_row();
-	break;
-case 'edit_row':
-	edit_row();
-	break;
+    case 'init_data_list' :
+        init_data_list();
+        break;
+    case 'add_row' :
+        add_row();
+        break;
+    case 'del_row' :
+        del_row();
+        break;
+    case 'edit_row' :
+        edit_row();
+        break;
 }
 
 function init_data_list() {
     $sql = "SELECT * FROM `et_data`";
     $query = query_sql($sql);
-    while($row = $query->fetch_assoc()){
-		$data[] = $row;
-	}
+    while ($row = $query -> fetch_assoc()) {
+        $data[] = $row;
+    }
     echo json_encode($data);
 }
 
@@ -38,25 +38,45 @@ function add_row() {
     $sql = "INSERT INTO `et_data` (`c_a`,`c_b`,`c_c`,`c_d`,`c_e`,`c_f`,`c_g`,`c_h`) VALUES( ";
     for ($i = 0; $i < 8; $i++) {
         $sql .= '\'' . $_POST['col_' . $i] . '\', ';
-        
+
     }
     $sql = trim($sql, ', ');
     $sql .= ")";
-    
-    if (query_sql($sql)) {
-        echo 'ok';
+
+    if ($res = query_sql($sql, "SELECT LAST_INSERT_ID() as LD")) {
+        $d = $res -> fetch_assoc();
+        echo $d['LD'];
     } else {
         echo 'db error ...';
     }
 }
 
+function edit_row() {
+    $sql = 'UPDATE `et_data` SET';
+    $id = $_POST['id'];
+    unset($_POST['id']);
+    // 释放变量
+    for ($i = 0; $i < 8; $i++) {
+        $sql .= '`c_' . chr(97 + $i) . '` = \'' . $_POST['col_' . $i] . '\' ,';
+
+    }
+    $sql = trim($sql, ', ');
+    $sql .= ' WHERE `id` = ' . $id;
+    
+    if(query_sql($sql)){
+        echo "ok";  
+    } else {
+        echo "db error ...";    
+    }
+}
+
 // 连接数据库
-function query_sql(){
-	$mysqli = new mysqli("127.0.0.1", "root", "", "etable");
-	$sqls = func_get_args();
-	foreach($sqls as $s){
-		$query = $mysqli->query($s);
-	}
-	$mysqli->close();
-	return $query;
+function query_sql() {
+    $mysqli = new mysqli("127.0.0.1", "root", "", "etable");
+    $sqls = func_get_args();
+    foreach ($sqls as $s) {
+        $query = $mysqli -> query($s);
+    }
+    $mysqli -> close();
+    return $query;
 }
--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
     var g_table = $("table.data");
     var data_url = 'data.php?action=init_data_list';
 
-    $.get(data_url, function(data) {
+    $.get(data_url, function(data) { // 初始化get数据
 
         var row_items = $.parseJSON(data);
 
@@ -28,6 +28,56 @@ $(function() {
         })
     }
 
+    function editHandler() {
+        var data_id = $(this).attr('dataid');
+        var meBtn = $(this);
+        var meRow = $(this).parent().parent();
+        var editRow = $("<tr></tr>");
+
+        // 往里面塞数据
+        for(var i = 0; i < 8; i++) {
+            var editTd = $("<td><input type='text' class='txtField' /></td>");
+            var v = meRow.find('td:eq(' + i + ')').html();
+            editTd.find('input').val(v);
+            editRow.append(editTd);
+        }
+
+        var opt_td = $("<td></td>");
+        var saveBtn = $("<a href='javascript:;' class='optLink'>保存&nbsp;</a>");
+
+        saveBtn.click(function() {
+            var currentRow = $(this).parent().parent();
+            var input_field = currentRow.find('input');
+            var post_field = {};
+            for(var i = 0, j = input_field.length; i < j; i++) {
+                post_field['col_' + i] = input_field[i].value; // input取了[i]之后就是普通的dom对象了，所以取值只能用value
+            }
+            post_field['id'] = data_id;
+            $.post("data.php?action=edit_row", post_field, function(res) {
+                if (res == "ok") {
+                    var newUpdateRow = create_row(post_field);
+                    currentRow.replaceWith(newUpdateRow);
+                } else {
+                    alert("数据更新失败...")
+                }
+            });
+        });
+
+        var cancelBtn = $("<a href='javascript:;' class='optLink'>取消&nbsp;</a>");
+
+        cancelBtn.click(function() {
+            var currentRow = $(this).parent().parent();
+            meRow.find('a:eq(0)').click(delHandler);
+            meRow.find('a:eq(1)').click(editHandler);
+            currentRow.replaceWith(meRow); // meRow里面是没有事件的需要重新绑定事件
+        });
+
+        opt_td.append(saveBtn);
+        opt_td.append(cancelBtn);
+        editRow.append(opt_td);
+        meRow.replaceWith(editRow);
+    }
+
     function create_row(data_item) {
         var row_obj = $("<tr></tr>");
         for(var k in data_item) {
@@ -39,14 +89,19 @@ $(function() {
         }
 
         var del_btn = $("<a class='optLink' href='javascript:;'>删除&nbsp;</a>");
-        var add_btn = $("<a class='optLink' href='javascript:;'>添加&nbsp;</a>");
+
+        del_btn.attr('dataid', data_item['id']);
+        del_btn.click(delHandler);
+
+        var edit_btn = $("<a class='optLink' href='javascript:;'>编辑&nbsp;</a>");
+
+        edit_btn.attr('dataid', data_item['id']);
+        edit_btn.click(editHandler);
+
         var opt_td = $("<td></td>");
 
-        $(del_btn).attr('dataid', data_item['id']);
-        $(del_btn).click(delHandler);
-
         $(opt_td).append(del_btn);
-        $(opt_td).append(add_btn);
+        $(opt_td).append(edit_btn);
 
         row_obj.append(opt_td);
         return row_obj;
@@ -63,22 +118,28 @@ $(function() {
         var col_opt = $("<td></td>");
 
         var confirmBtn = $("<a href='javascript:;' class='optLink'>确认&nbsp;</a>");
-        
-        confirmBtn.click(function (){
+
+        confirmBtn.click(function() {
             var currentRow = $(this).parent().parent();
             var input_field = currentRow.find('input');
             var post_field = {}
-            for (var i = 0, j = input_field.length; i < j; i++) {
-                post_field['col_' + i] = input_field[i].value;
+            for(var i = 0, j = input_field.length; i < j; i++) {
+                post_field['col_' + i] = input_field[i].value; // input取了[i]之后就是普通的dom对象了，所以取值只能用value
             }
-            $.post("data.php?action=add_row", post_field, function (res){
-               
+            $.post("data.php?action=add_row", post_field, function(res) {
+                if(res > 0) {
+                    post_field['id'] = res;
+                    var postAddRow = create_row(post_field);
+                    currentRow.replaceWith(postAddRow);
+                } else {
+                    alert('插入失败...')
+                }
             });
         });
-        
+
         var cancelBtn = $("<a href='javascript:;' class='optLink'>取消&nbsp;</a>");
-        
-        cancelBtn.click(function (){
+
+        cancelBtn.click(function() {
             $(this).parent().parent().remove('tr');
         });
 
@@ -88,7 +149,5 @@ $(function() {
         addRow.append(col_opt);
         g_table.append(addRow);
     });
-    
-    
 
 });
